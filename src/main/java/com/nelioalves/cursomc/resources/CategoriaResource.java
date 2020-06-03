c67b423a87354748411207026a0b664c836b6fda
Alexander Santos Andrade <al.praia@gmail.com>
2020-06-02 21:03:04 -0300

    reinstalado

---
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.nelioalves.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/{id}")
+	//@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.buscar(id);
+		
+		return ResponseEntity.ok(obj);
+	}
+}
